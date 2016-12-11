3584b5a1fddef22b0a4ff8453958a58923035c8b
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-11 11:16:34 -0600

    Split out Interceptor and Protocols

--- Miruken/Protocol.cs
+++ Miruken/Protocol.cs
@@ -1,74 +1,7 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.Remoting;
-using System.Runtime.Remoting.Messaging;
-using System.Runtime.Remoting.Proxies;
-using Miruken.Callback;
+﻿using Miruken.Callback;
 
 namespace Miruken
 {
-    public interface IStrict {}
-
-    public interface IProtocolAdapter
-    {
-        object Dispatch(IMethodCallMessage message);
-    }
-
-    #region NullProtocolAdapter
-
-    public class NullProtocolAdapter : IProtocolAdapter
-    {
-        public static readonly NullProtocolAdapter
-            Instance = new NullProtocolAdapter();
-
-        private NullProtocolAdapter()
-        {
-        }
-
-        public object Dispatch(IMethodCallMessage message)
-        {
-            return null;
-        }
-    }
-
-    #endregion
-
-    public class Interceptor : RealProxy, IRemotingTypeInfo
-    {
-        private readonly IProtocolAdapter _adapter;
-
-        public Interceptor(IProtocolAdapter adapter)
-            : base(typeof(MarshalByRefObject))
-        {
-            if (adapter == null)
-                throw new ArgumentNullException(nameof(adapter));
-            _adapter = adapter;
-        }
-
-        public string TypeName { get; set; }
-
-        public bool CanCastTo(Type fromType, object o)
-        {
-            return true;
-        }
-
-        public override IMessage Invoke(IMessage msg)
-        {
-            var methodCall = (IMethodCallMessage) msg;
-            try
-            {
-                return new ReturnMessage(
-                    _adapter.Dispatch(methodCall),
-                    methodCall.Args, methodCall.ArgCount,
-                    methodCall.LogicalCallContext, methodCall);
-            }
-            catch (TargetInvocationException tex)
-            {
-                return new ReturnMessage(tex.InnerException, methodCall);
-            }
-        }
-    }
-
     public static class Protocol
     {
         public static object P(IProtocolAdapter adapter)
@@ -78,7 +11,7 @@ namespace Miruken
 
         public static TProtocol P<TProtocol>(IProtocolAdapter adapter)
         {
-            return (TProtocol) new Interceptor(adapter).GetTransparentProxy();
+            return (TProtocol)new Interceptor(adapter).GetTransparentProxy();
         }
 
         public static object As(this IProtocolAdapter adapter)
