be657612f5e7c86cf2f6c9d3c64431f159128bb4
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-14 09:45:35 -0600

    Added PropertyChangedExtensions

---
+++ Miruken.Tests/Infrastructure/PropertyChangeExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Miruken.Tests.Infrastructure
+{
+    using System.ComponentModel;
+    using Miruken.Infrastructure;
+
+    [TestClass]
+    public class PropertyChangeExtensionsTests
+    {
+        public class Person
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string FirstName
+            {
+                get { return _firstName; }
+                set { PropertyChanged.ChangeProperty(ref _firstName, value, this); }
+            }
+            private string _firstName;
+        }
+
+        [TestMethod]
+        public void Should_Notify_Property_Changed()
+        {
+            var called = false;
+            var person = new Person();
+            person.PropertyChanged += (sender, e) =>
+            {
+                called = true;
+                Assert.AreEqual("FirstName", e.PropertyName);
+            };
+            person.FirstName = "Donald";
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void Should_Ignore_Property_Changed_If_Same()
+        {
+            var called = false;
+            var person = new Person { FirstName = "Donald" };
+            person.PropertyChanged += (sender, e) =>
+            {
+                called = true;
+            };
+            person.FirstName = "Donald";
+            Assert.IsFalse(called);
+        }
+    }
+}
---
+++ Miruken/Infrastructure/PropertyChangedExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Miruken.Infrastructure
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public static class PropertyChangedExtensions
+    {
+        public static bool ChangeProperty<T>(
+            this PropertyChangedEventHandler propertyChanged,
+            ref T field, T value, object sender, IEqualityComparer<T> comparer = null, 
+            [CallerMemberName] string propertyName = null)
+        {
+            if (comparer == null)
+                comparer = EqualityComparer<T>.Default;
+
+            if (comparer.Equals(field, value))
+                return false;
+
+            field = value;
+            propertyChanged?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
