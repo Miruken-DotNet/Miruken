ae6c8577905e49564b680b5a8858d46f5603c000
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-19 17:19:31 -0600

    Added ContextualTests

---
+++ Miruken.Tests/Context/ContextualTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Miruken.Tests.Context
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Miruken.Callback;
+    using Miruken.Context;
+
+    [TestClass]
+    public class ContextualTests
+    {
+        private Context _rootContext;
+
+        public class MyService : Contextual
+        {
+            
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _rootContext = new Context();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _rootContext.End();
+        }
+
+        [TestMethod]
+        public void Should_Add_Contextual_To_Context_When_Assigned()
+        {
+            var service  = new MyService { Context = _rootContext };
+            var services = _rootContext.ResolveAll<MyService>();
+            Assert.AreEqual(1, services.Length);
+        }
+    }
+}
