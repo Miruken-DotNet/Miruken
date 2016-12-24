f57270ddc12d60bc914746f401f655bd3f288cb1
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-24 08:49:27 -0600

    Added ChangeProperty overload for EventHandler<PropertyChangedEventArgs>

--- Miruken/Infrastructure/PropertyChangedExtensions.cs
+++ Miruken/Infrastructure/PropertyChangedExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace Miruken.Infrastructure
 {
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
@@ -10,6 +11,22 @@
             this PropertyChangedEventHandler propertyChanged,
             ref T field, T value, object sender, IEqualityComparer<T> comparer = null, 
             [CallerMemberName] string propertyName = null)
+        {
+            if (comparer == null)
+                comparer = EqualityComparer<T>.Default;
+
+            if (comparer.Equals(field, value))
+                return false;
+
+            field = value;
+            propertyChanged?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
+        public static bool ChangeProperty<T>(
+             this EventHandler<PropertyChangedEventArgs> propertyChanged,
+             ref T field, T value, object sender, IEqualityComparer<T> comparer = null,
+             [CallerMemberName] string propertyName = null)
         {
             if (comparer == null)
                 comparer = EqualityComparer<T>.Default;
