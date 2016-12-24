e9d6881eca3a87b759f94cefeb7c7ec495007b5b
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-24 12:06:13 -0600

    Added ContextualHandler

---
+++ Miruken/Context/ContextualHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Miruken.Context
+{
+    using Callback;
+
+    public class ContextualHandler : Handler, IContextual
+    {
+        private IContext _context;
+
+        public IContext Context
+        {
+            get { return _context; }
+            set
+            {
+                if (_context == value) return;
+                var newContext = value;
+                ContextChanging?.Invoke(this, _context, ref newContext);
+                _context?.RemoveHandlers(this);
+                var oldContext = _context;
+                _context = newContext;
+                _context?.InsertHandlers(0, this);
+                ContextChanged?.Invoke(this, oldContext, _context);
+            }
+        }
+
+        public event ContextChangingDelegate<IContext> ContextChanging;
+        public event ContextChangedDelegate<IContext> ContextChanged;
+    }
+}
