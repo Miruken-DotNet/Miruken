676d45b16a2ffa8f9f9f57c0e63cd147b71ca194
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-06-15 12:52:14 -0500

    Added ValidationInstaller

---
+++ Source/Miruken.Castle/ContravariantFilter.cs
@@ -0,0 +1,26 @@
+﻿namespace Miruken.Castle
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using global::Castle.MicroKernel;
+
+    public class ContravariantFilter : IHandlersFilter
+    {
+        public bool HasOpinionAbout(Type service)
+        {
+            if (!service.IsGenericType) return false;
+            var genericType      = service.GetGenericTypeDefinition();
+            var genericArguments = genericType.GetGenericArguments();
+            return genericArguments.Count(arg => arg.GenericParameterAttributes
+                .HasFlag(GenericParameterAttributes.Contravariant)) == 1;
+        }
+
+        public IHandler[] SelectHandlers(Type service, IHandler[] handlers)
+        {
+            return handlers // prefer concrete handlers
+                .Where(h => !h.ComponentModel.Implementation.IsGenericTypeDefinition)
+                .ToArray();
+        }
+    }
+}
--- Source/Miruken.Validate.Castle/ValidationInstaller.cs
+++ Source/Miruken.Validate.Castle/ValidationInstaller.cs
@@ -1,17 +1,39 @@
 ﻿namespace Miruken.Validate.Castle
 {
+    using System;
     using global::Castle.MicroKernel.Registration;
     using global::Castle.MicroKernel.SubSystems.Configuration;
     using global::Castle.Windsor;
     using global::FluentValidation;
+    using Miruken.Castle;
 
-    public class ValidationInstaller : IWindsorInstaller
+    public class ValidationInstaller : PluginInstaller
     {
-        public void Install(IWindsorContainer container, IConfigurationStore store)
+        private IWindsorContainer _container;
+        private Action<ComponentRegistration> _configure;
+
+        public ValidationInstaller ConfigureValidators(Action<ComponentRegistration> configure)
         {
-            container.Register(Component.For<IValidatorFactory>()
+            _configure += configure;
+            return this;
+        }
+
+        protected override void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            _container = container;
+            _container.Register(Component.For<IValidatorFactory>()
                 .ImplementedBy<WindsorValidatorFactory>()
                 .OnlyNewServices());
+        }
+
+        protected override void InstallPlugin(Plugin plugin)
+        {
+            var validators = Classes.FromAssembly(plugin.Assembly)
+                .BasedOn(typeof(IValidator<>))
+                .WithServiceBase();
+            if (_configure != null)
+                validators.Configure(_configure);
+            _container.Register(validators);
         }
     }
 }
