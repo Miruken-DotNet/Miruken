1e583b28ceb122a574084d3a8cc6a4f4df32cd24
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-06-16 08:14:20 -0500

    Added Plugins.FromAssemblies and Plugins.FromAssemblyNames

--- Source/Miruken.Castle/Plugins.cs
+++ Source/Miruken.Castle/Plugins.cs
@@ -1,12 +1,29 @@
 ﻿namespace Miruken.Castle
 {
     using System.Linq;
+    using System.Reflection;
     using global::Castle.Core.Internal;
     using global::Castle.MicroKernel.Registration;
     using global::Castle.Windsor.Installer;
 
     public static class Plugins
     {
+        public static IWindsorInstaller FromAssemblies(params Assembly[] assemblies)
+        {
+            var installer = new CompositeInstaller();
+            foreach (var assembly in assemblies)
+                installer.Add(Plugin.FromAssembly(assembly));
+            return installer;
+        }
+
+        public static IWindsorInstaller FromAssembliesNamed(params string[] assemblyNames)
+        {
+            var installer = new CompositeInstaller();
+            foreach (var assemblyName in assemblyNames)
+                installer.Add(Plugin.FromAssemblyNamed(assemblyName));
+            return installer;
+        }
+
         public static IWindsorInstaller InDirectory(AssemblyFilter filter)
         {
             var installer = new CompositeInstaller();
