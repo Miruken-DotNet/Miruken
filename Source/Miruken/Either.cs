17158f64ef89c09e501e7b75a9cd2a0b9e56652a
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-08-31 12:39:31 -0500

    Added Try specialization of Either

--- Source/Miruken/Either.cs
+++ Source/Miruken/Either.cs
@@ -2,7 +2,13 @@
 {
     using System;
 
-    public class Either<TL, TR>
+    public interface IEither
+    {
+        void Match(Action<object> matchLeft, Action<object> matchRight);
+        object Match(Func<object, object> matchLeft, Func<object, object> matchRight);
+    }
+
+    public class Either<TL, TR> : IEither
     {
         private readonly TL _left;
         private readonly TR _right;
@@ -34,7 +40,32 @@
                 matchRight(_right);
         }
 
+        void IEither.Match(Action<object> matchLeft, Action<object> matchRight)
+        {
+            if (matchLeft == null)
+                throw new ArgumentNullException(nameof(matchLeft));
+
+            if (matchRight == null)
+                throw new ArgumentNullException(nameof(matchRight));
+
+            if (_isLeft)
+                matchLeft(_left);
+            else
+                matchRight(_right);
+        }
+
         public T Match<T>(Func<TL, T> matchLeft, Func<TR, T> matchRight)
+        {
+            if (matchLeft == null)
+                throw new ArgumentNullException(nameof(matchLeft));
+
+            if (matchRight == null)
+                throw new ArgumentNullException(nameof(matchRight));
+
+            return _isLeft ? matchLeft(_left) : matchRight(_right);
+        }
+
+        object IEither.Match(Func<object, object> matchLeft, Func<object, object> matchRight)
         {
             if (matchLeft == null)
                 throw new ArgumentNullException(nameof(matchLeft));
@@ -51,5 +82,4 @@
         public static implicit operator Either<TL, TR>(TL left) => new Either<TL, TR>(left);
         public static implicit operator Either<TL, TR>(TR right) => new Either<TL, TR>(right);
     }
-
 }
---
+++ Source/Miruken/Try.cs
@@ -0,0 +1,33 @@
+﻿namespace Miruken
+{
+    using System;
+
+    public class Try<TE, TR> : Either<TE, TR>
+    {
+        public Try(TR result)
+             : base(result)
+        {
+        }
+
+        public Try(TE error)
+            : base(error)
+        {
+            IsError = true;
+        }
+
+        public bool IsError { get; }
+    }
+
+    public class Try<TR> : Try<Exception, TR>
+    {
+        public Try(TR result)
+             : base(result)
+        {
+        }
+
+        public Try(Exception exception)
+        : base(exception)
+        {
+        }
+    }
+}
