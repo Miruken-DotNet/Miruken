92ffd61f7ce381c35f7e96482846bb93879d5bf0
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-08-31 19:36:14 -0500

    Removed Try single generic

--- Source/Miruken/Try.cs
+++ Source/Miruken/Try.cs
@@ -1,7 +1,5 @@
 ﻿namespace Miruken
 {
-    using System;
-
     public class Try<TE, TR> : Either<TE, TR>
     {
         public Try(TR result)
@@ -19,19 +17,5 @@
 
         public static implicit operator Try<TE, TR>(TE error) => new Try<TE, TR>(error);
         public static implicit operator Try<TE, TR>(TR result) => new Try<TE, TR>(result);
-
-    }
-
-    public class Try<TR> : Try<Exception, TR>
-    {
-        public Try(TR result)
-             : base(result)
-        {
-        }
-
-        public Try(Exception exception)
-        : base(exception)
-        {
-        }
     }
 }
