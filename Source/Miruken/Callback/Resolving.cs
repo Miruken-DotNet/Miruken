df14b2045c31b60b9f80f4758447eacefc9b029b
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-09-26 21:57:01 -0500

    Another test

--- Source/Miruken/Callback/Resolving.cs
+++ Source/Miruken/Callback/Resolving.cs
@@ -35,8 +35,8 @@
         {
             var handlers = CallbackPolicy.GetCallbackHandlers(callback).ToArray();
             if (handlers.Length == 0) return callback;
-            var bundle = new Bundle(false);
-                //.Add(h => h.Handle(callback), handled => handled);
+            var bundle = new Bundle(false)
+                .Add(h => h.Handle(callback), handled => handled);
             foreach (var handler in handlers)
                 bundle.Add(h => h.Handle(new Resolving(handler, callback)));
             return bundle;
