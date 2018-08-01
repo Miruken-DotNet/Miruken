d52ce6d740df812a6ce7adf48eebcd68b1622f87
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2018-08-01 18:32:39 -0500

    Added ClaimsAttribute resolver

---
+++ Source/Miruken.Secure/ClaimAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace Miruken.Secure
+{
+    using System;
+    using System.Security.Claims;
+    using System.Security.Principal;
+    using Callback;
+    using Callback.Policy;
+    using Infrastructure;
+
+    public class ClaimAttribute : Attribute, IArgumentResolver
+    {
+        public ClaimAttribute(string claimType)
+        {
+            if (string.IsNullOrEmpty(claimType))
+                throw new ArgumentException("ClaimType cannot be empty");
+            ClaimType = claimType;
+        }
+
+        public string ClaimType { get; }
+
+        public bool IsOptional => false;
+
+        public void ValidateArgument(Argument argument)
+        {
+            if (!argument.ParameterType.IsSimpleType())
+                throw new NotSupportedException(
+                    "Claim parameters must be simple types");
+        }
+
+        public object ResolveArgument(
+            Argument argument, IHandler handler, IHandler composer)
+        {
+            var principal = composer.Resolve<IPrincipal>();
+            if (!(principal?.Identity is ClaimsIdentity identity))
+                throw new RejectedException();
+            var claim = identity.FindFirst(ClaimType);
+            return claim == null ? throw new RejectedException()
+                 : RuntimeHelper.ChangeType(claim.Value, argument.ParameterType);
+        }
+    }
+}
--- Source/Miruken/Callback/RejectedException.cs
+++ Source/Miruken/Callback/RejectedException.cs
@@ -4,6 +4,15 @@ namespace Miruken.Callback
 {
     public class RejectedException : CancelledException
     {
+        public RejectedException()
+        {       
+        }
+
+        public RejectedException(string message)
+            : base(message)
+        {          
+        }
+
         public RejectedException(object callback)
             : base("Callback has been cancelled")
         {
