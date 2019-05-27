07212cc3ee36195e2b6cac0cf345c40fc7cad18e
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-05-27 14:30:39 -0500

    Added IKeyProperties extensions

--- Source/Miruken/Api/KeyExtensions.cs
+++ Source/Miruken/Api/KeyExtensions.cs
@@ -19,5 +19,15 @@
         {
             return keys.FirstOrDefault(k => Equals(k.Id, id)) ?? new Key<T>(id);
         }
+
+        public static string FindNameOrDefault<T>(this IEnumerable<IKeyProperties<T>> keyProperties, T id)
+        {
+            return keyProperties.FirstOrDefault(x => Equals(x.Id, id))?.Name ?? string.Empty;
+        }
+
+        public static string FindNameOrDefault<T>(this IEnumerable<Key<T>> keys, T id)
+        {
+            return keys.FirstOrDefault(k => Equals(k.Id, id))?.Name ?? string.Empty;
+        }
     }
 }
