292890a0b66547fe2ff9130e22d1c8b21ff20695
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-07-15 07:55:21 -0500

    Added DelegatingOnceStrategy

---
+++ Test/Miruken.Tests/Api/Once/DelegatingOnceStrategyTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Miruken.Tests.Api.Once
+{
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Miruken.Api;
+    using Miruken.Api.Once;
+    using Miruken.Callback;
+    using Miruken.Callback.Policy;
+    using Miruken.Map;
+
+    [TestClass]
+    public class DelegatingOnceStrategyTests
+    {
+        private IHandler _handler;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var factory = new MutableHandlerDescriptorFactory();
+            factory.RegisterDescriptor<FooHandler>();
+            factory.RegisterDescriptor<OnceHandler>();
+            factory.RegisterDescriptor<Provider>();
+            HandlerDescriptorFactory.UseFactory(factory);
+
+            _handler = new FooHandler() + new OnceHandler();
+        }
+
+        [TestMethod]
+        public async Task Should_Delegate_Once()
+        {
+            var foo  = new Foo().Once();
+            var once = await _handler.Send<Once>(foo);
+            Assert.AreEqual(foo.RequestId, once.RequestId);
+        }
+
+        private class Foo { }
+
+        private class FooHandler : Handler
+        {
+            [Handles]
+            public Once HandleFoo(Foo foo, Once once)
+            {
+                return once;
+            }
+
+            [Maps]
+            public IOnceStrategy Once(Foo foo)
+            {
+                return DelegatingOnceStrategy.Instance;
+            }
+        }
+    }
+}
