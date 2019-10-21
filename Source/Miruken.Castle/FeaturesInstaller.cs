bf5ceece84e61bec652cd48951cc67fc0d2c1732
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-10-21 11:35:48 -0500

    Added Miruken.Castle and Miruken.Validate.Castle back

---
+++ Source/Miruken.Castle/FeaturesInstaller.cs
@@ -0,0 +1,76 @@
+﻿namespace Miruken.Castle
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using global::Castle.MicroKernel.Registration;
+    using global::Castle.MicroKernel.SubSystems.Configuration;
+    using global::Castle.Windsor;
+
+    public class FeaturesInstaller : IWindsorInstaller
+    {
+        private readonly List<FeatureInstaller> _features;
+        private readonly List<FromDescriptor> _from;
+        private bool _useExternalDependencies;
+        private IWindsorContainer _container;
+
+        public FeaturesInstaller(params FeatureInstaller[] features)
+        {
+            _features = new List<FeatureInstaller>();
+            _from     = new List<FromDescriptor>();
+            Add(features);
+        }
+
+        public FeaturesInstaller WithExternalDependencies()
+        {
+            _useExternalDependencies = true;
+            return this;
+        }
+
+        public FeaturesInstaller Add(params FeatureInstaller[] features)
+        {
+            _features.AddRange(features);
+            return this;
+        }
+
+        public FeaturesInstaller Use(params FromDescriptor[] from)
+        {
+            if (_container == null)
+                _from.AddRange(from);
+            else
+                foreach (var f in from)
+                    InstallFeatures(f);
+            return this;
+        }
+
+        void IWindsorInstaller.Install(
+            IWindsorContainer container, IConfigurationStore store)
+        {
+            if (Interlocked.CompareExchange(
+                ref _container, container, null) != null)
+                return;
+
+            if (_useExternalDependencies)
+                _container.Kernel.Resolver.AddSubResolver(new ExternalDependencyResolver());
+
+            if (_features.Count == 0) return;
+            var implied = new List<FromDescriptor>();
+
+            foreach (var feature in _features)
+            {
+                ((IWindsorInstaller)feature).Install(container, store);
+                implied.AddRange(feature.GetFeatures());
+            }
+
+            foreach (var from in _from.Concat(implied))
+                InstallFeatures(from);
+        }
+
+        private void InstallFeatures(FromDescriptor from)
+        {
+            foreach (var feature in _features)
+                feature.InstallFeatures(from);
+            _container.Register(from);
+        }
+    }
+}
---
+++ Source/Miruken.Castle/WindsorHandler.cs
@@ -0,0 +1,88 @@
+﻿namespace Miruken.Castle
+{
+    using System;
+    using System.Collections;
+    using global::Castle.MicroKernel;
+    using global::Castle.Windsor;
+    using Callback;
+    using global::Castle.MicroKernel.Handlers;
+    using global::Castle.MicroKernel.Resolvers.SpecializedResolvers;
+    using IHandler = Callback.IHandler;
+
+    public class WindsorHandler : Handler, IDisposable
+    {
+        public WindsorHandler(Action<IWindsorContainer> config = null)
+        {
+            Container = new WindsorContainer();
+            config?.Invoke(Container);
+            ConfigureWindsor();
+        }
+
+        public WindsorHandler(IWindsorContainer container)
+        {
+            Container = container;
+            ConfigureWindsor();
+        }
+
+        private void ConfigureWindsor()
+        {
+            Container.Kernel.Resolver.AddSubResolver(
+                new CollectionResolver(Container.Kernel, true));
+        }
+
+        public IWindsorContainer Container { get; }
+
+        [Provides]
+        private object Resolve(Inquiry inquiry, IHandler composer)
+        {
+            var type = inquiry.Key as Type;
+            if (type == null || type.IsGenericTypeDefinition)
+                return null;
+
+            try
+            {
+                var args = CreateArgs(composer, inquiry);
+                return inquiry.Many || inquiry is Resolving
+                     ? Container.ResolveAll(type, args)
+                     : Resolve(type, args);
+            }
+            catch (ComponentNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Container?.Dispose();
+        }
+
+        private object Resolve(Type type, IDictionary args)
+        {
+            try
+            {
+                return Container.Resolve(type, args);
+            }
+            catch (GenericHandlerTypeMismatchException)
+            {
+                // Generic type constraints are not handled out
+                // of the box.  IHandlerSelector can be used
+                // but will throw this exception if no handlers
+                // are returned.
+                return null;
+            }
+        }
+        private static IDictionary CreateArgs(IHandler composer, Inquiry inquiry)
+        {
+            composer = composer ?? Composer;
+            return composer == null ? null : new Hashtable
+            {
+                [ResolutionKey] = inquiry,
+                [ComposerKey]   = composer
+            };
+        }
+
+        internal static readonly object ResolutionKey = new object();
+        internal static readonly object ComposerKey   = new object();
+    }
+}
