e39d7ac44faf0b06fe494c017906c732390fe3ec
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-10-30 09:21:03 -0500

    Added missing log tests

---
+++ Test/Miruken.Tests/Log/LogFilterTests.cs
@@ -0,0 +1,222 @@
+﻿#if NETSTANDARD
+namespace Miruken.Tests.Log
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Miruken.Api;
+    using Miruken.Callback;
+    using Miruken.Callback.Policy.Bindings;
+    using Miruken.Concurrency;
+    using Miruken.Log;
+    using Miruken.Register;
+    using NLog;
+    using NLog.Config;
+    using NLog.Extensions.Logging;
+    using NLog.Targets;
+
+    [TestClass]
+    public class LogFilterTests
+    {
+        protected LoggingConfiguration _loggingConfig;
+        protected MemoryTarget _memoryTarget;
+        private IHandler _handler;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _memoryTarget = new MemoryTarget
+            {
+                Layout = "${level:uppercase=true} ${logger} ${message} ${exception:format=tostring}"
+            };
+            _loggingConfig = new LoggingConfiguration();
+            _loggingConfig.AddTarget("InMemoryTarget", _memoryTarget);
+            _loggingConfig.LoggingRules.Add(new LoggingRule("*", NLog.LogLevel.Debug, _memoryTarget));
+            LogManager.Configuration = _loggingConfig;
+
+            _handler = new ServiceCollection()
+                .AddLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    logging.AddNLog();
+                })
+                .AddMiruken(configure => configure
+                    .PublicSources(sources => sources.AddTypes(
+                        typeof(CallbackHandler), typeof(BazHandler), 
+                        typeof(StockMarket), typeof(ConsoleFilter)))
+                );
+        }
+
+        [TestMethod]
+        public void Should_Log_Callbacks()
+        {
+            var handled = _handler.Handle(new Foo());
+            Assert.IsTrue(handled);
+
+            var events = _memoryTarget.Logs;
+            Assert.AreEqual(4, events.Count);
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.CallbackHandler.*Handling Foo").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.CallbackHandler.*Handling Bar").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.CallbackHandler.*Completed Bar").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.CallbackHandler.*Completed Foo").Success));
+        }
+
+        [TestMethod]
+        public async Task Should_Log_Unhandled_Callbacks()
+        {
+            await _handler.Chain(new BazHandler()).Send(new Baz());
+
+            var events = _memoryTarget.Logs;
+            Assert.AreEqual(4, events.Count);
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.CallbackHandler.*Handling Baz").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"ERROR.*System.NotSupportedException Miruken\.Tests\.Log\.LogFilterTests\+CallbackHandler Failed Baz.*System\.NotSupportedException: Miruken\.Concurrency\.Promise Handle\(Baz\) not handled").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.BazHandler.*Handling Baz").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.BazHandler.*Completed Baz").Success));
+        }
+
+        [TestMethod]
+        public void Should_Log_Methods()
+        {
+            var cost = _handler.Proxy<IStockMarket>().Buy("MSFT", 3);
+            Assert.AreEqual(426, cost);
+
+            var events = _memoryTarget.Logs;
+            Assert.AreEqual(2, events.Count);
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.StockMarket.*Handling HandleMethod").Success));
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @"DEBUG.*Miruken\.Tests\.Log\.LogFilterTests\.StockMarket.*Completed HandleMethod.*with decimal").Success));
+        }
+
+        [TestMethod]
+        public void Should_Log_Exceptions_As_Error()
+        {
+            try
+            {
+                _handler.Handle(new Bad(new ArgumentException("Bad")));
+                Assert.Fail("Should not get here");
+            }
+            catch
+            {
+                var events = _memoryTarget.Logs;
+                Assert.AreEqual(2, events.Count);
+                Assert.IsTrue(events.Any(x => Regex.Match(x,
+                    @"ERROR.*System\.ArgumentException.*Miruken\.Tests\.Log\.LogFilterTests\+CallbackHandler Failed Bad").Success));
+            }
+        }
+
+        [TestMethod]
+        public void Should_Filter_Exceptions()
+        {
+             _loggingConfig.LoggingRules.Insert(0, new LoggingRule(
+                 "System.InvalidOperationException", NLog.LogLevel.Trace, null)
+             {
+                 Final = true
+             });
+
+            try
+            {
+                _handler.Handle(new Bad(new InvalidOperationException("Something crashed")));
+                Assert.Fail("Should not get here");
+            }
+            catch
+            {
+                var events = _memoryTarget.Logs;
+                Assert.AreEqual(1, events.Count);
+                Assert.IsFalse(events.Any(x => Regex.Match(x,
+                    @"ERROR.*System\.InvalidOperationException.*Miruken\.Tests\.Log\.LogFilterTests\+CallbackHandler Failed Bad").Success));
+            }
+        }
+
+        public class Foo { }
+        public class Bar { }
+        public class Baz { }
+
+        public class Bad
+        {
+            public Bad(Exception exception)
+            {
+                Exception = exception;
+            }
+
+            public Exception Exception { get; }
+        }
+
+        public class CallbackHandler : Handler
+        {
+            [Handles, Log, Filter(typeof(ConsoleFilter))]
+            public void Handle(Foo foo)
+            {
+            }
+
+            [Handles, Log, Filter(typeof(ConsoleFilter))]
+            public void Handle(Bar bar)
+            {
+            }
+
+            [Handles, Log, Filter(typeof(ConsoleFilter))]
+            public Promise Handle(Baz baz)
+            {
+                return null;
+            }
+
+            [Handles, Log, Filter(typeof(ConsoleFilter))]
+            public void Handle(Bad bad)
+            {
+                throw bad.Exception;
+            }
+        }
+
+        [Unmanaged]
+        public class BazHandler : Handler
+        {
+            [Handles, Log, Filter(typeof(ConsoleFilter))]
+            public void Handle(Baz baz)
+            {
+            }
+        }
+
+        public interface IStockMarket : IResolving
+        {
+            decimal Buy(string symbol, int quantity);
+        }
+
+        public class StockMarket : Handler, IStockMarket
+        {
+            [Log]
+            public decimal Buy(string symbol, int quantity)
+            {
+                return 142 * quantity;
+            }
+        }
+
+        public class ConsoleFilter : IFilter<Foo, object>
+        {
+            public int? Order { get; set; }
+
+            public Task<object> Next(Foo callback,
+                object rawCallback, MemberBinding member, 
+                IHandler composer, Next<object> next,
+                IFilterProvider provider = null)
+            {
+                Console.WriteLine(callback);
+                composer.EnableFilters().Handle(new Bar());
+                return next();
+            }
+        }
+    }
+}
+#endif
---
+++ Test/Miruken.Tests/Log/LoggerProviderTests.cs
@@ -0,0 +1,131 @@
+﻿#if NETSTANDARD
+namespace Miruken.Tests.Log
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Miruken.Api;
+    using Miruken.Callback;
+    using Miruken.Callback.Policy.Bindings;
+    using Miruken.Concurrency;
+    using Miruken.Log;
+    using Miruken.Register;
+    using NLog;
+    using NLog.Config;
+    using NLog.Extensions.Logging;
+    using NLog.Targets;
+    using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+    [TestClass]
+    public class LoggerProviderTests
+    {
+        protected LoggingConfiguration _loggingConfig;
+        protected MemoryTarget _memoryTarget;
+        private IHandler _handler;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _memoryTarget = new MemoryTarget
+            {
+                Layout = "${level:uppercase=true} ${logger} ${message} ${exception:format=tostring}"
+            };
+            _loggingConfig = new LoggingConfiguration();
+            _loggingConfig.AddTarget("InMemoryTarget", _memoryTarget);
+            _loggingConfig.LoggingRules.Add(new LoggingRule("*", NLog.LogLevel.Debug, _memoryTarget));
+            LogManager.Configuration = _loggingConfig;
+
+            _handler = new ServiceCollection()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog();
+                })
+                .AddMiruken(configure => configure
+                    .PublicSources(sources => sources.AddTypes(
+                        typeof(Inventory), typeof(Billing)))
+                );
+        }
+
+        [TestMethod]
+        public void Should_Inject_Logger_Contextually()
+        {
+            var handled = _handler.Handle(new Order
+            {
+                PLU      = "871212",
+                Quantity = 3
+            });
+            Assert.IsTrue(handled);
+
+            var events = _memoryTarget.Logs;
+            Assert.AreEqual(1, events.Count);
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @".*DEBUG.*Miruken\.Tests\.Log\.LoggerProviderTests\.Inventory.*Received order for 3 '871212'").Success));
+        }
+
+        [TestMethod]
+        public void Should_Inject_Logger_Explicitly()
+        {
+            var handled = _handler.Handle(new Invoice { Amount = 150.30M });
+
+            Assert.IsTrue(handled);
+
+            var events = _memoryTarget.Logs;
+            Assert.AreEqual(1, events.Count);
+            Assert.IsTrue(events.Any(x => Regex.Match(x,
+                @".*DEBUG.*Miruken\.Tests\.Log\.LoggerProviderTests\.Billing.*Received invoice for \$150\.30").Success));
+        }
+
+        public class Order
+        {
+            public string PLU      { get; set; }
+            public int    Quantity { get; set; }
+        }
+
+        public class Invoice
+        {
+            public decimal Amount { get; set; }
+        }
+
+        public class Inventory : Handler
+        {
+            private readonly ILogger _logger;
+
+            public Inventory(ILogger logger)
+            {
+                _logger = logger;
+            }
+
+            [Handles]
+            public void Order(Order order)
+            {
+                _logger.LogDebug("Received order for {Quantity} '{PLU}'",
+                    order.Quantity, order.PLU);
+            }
+        }
+
+        public class Billing : Handler
+        {
+            private readonly ILogger<Billing> _logger;
+
+            public Billing(ILogger<Billing> logger)
+            {
+                _logger = logger;
+            }
+            
+            [Handles]
+            public void Invoice(Invoice invoice, ILogger<Billing> logger)
+            {
+                Assert.AreSame(_logger, logger);
+                _logger.LogDebug("Received invoice for {Amount}",
+                    invoice.Amount.ToString("C"));
+            }
+        }
+    }
+}
+#endif
