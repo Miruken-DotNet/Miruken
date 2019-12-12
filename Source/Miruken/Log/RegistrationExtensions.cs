7b033f421388fceb9ceb42abb9c0df788c44e1ab
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2019-12-12 13:24:23 -0600

    Added Log RegistrationExtensions

---
+++ Source/Miruken/Log/RegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿#if NETSTANDARD
+namespace Miruken.Log
+{
+    using Register;
+
+    public static class RegistrationExtensions
+    {
+        public static Registration WithLogging(this Registration registration)
+        {
+            if (!registration.CanRegister(typeof(RegistrationExtensions)))
+                return registration;
+
+            registration.AddFilters(new LogAttribute());
+
+            return registration;
+        }
+    }
+}
+#endif
